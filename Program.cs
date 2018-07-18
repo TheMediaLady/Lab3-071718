41a67c6a07e99e67c018bbaa81a017698841a582
Aquoinette <themedialady@blairunlimited.com>
2018-07-18 08:50:49 -0400

    Completed lab 3

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Lab3_071718
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Continue = "y";
+             
+             
+
+                Console.WriteLine("Greetings!! What is your name?");
+                string nameUsed = Console.ReadLine();
+            while(Continue == "y"){
+                Console.WriteLine($"Ok {nameUsed}, Please enter a whole number between 1 and 100.");
+                string numChose = Console.ReadLine();
+
+                int wholeNum = int.Parse(numChose);
+
+
+                if (wholeNum % 2 == 0)
+
+                {
+                    if (wholeNum >= 2 && wholeNum <= 25)
+                    {
+                        Console.WriteLine($"Thanks {nameUsed}!! You entered {numChose} and it's even and less than 25.");
+                        Console.ReadLine();
+                    }
+
+                    else if (wholeNum >= 25)
+                    {
+                        Console.WriteLine($"Thanks {nameUsed}!! You entered {numChose}");
+                        Console.ReadLine();
+                    }
+                }
+
+                else
+
+                    Console.WriteLine($"Thanks {nameUsed}!! You entered {numChose} and it's odd");
+                Console.ReadLine();
+                Console.WriteLine($"Would you like to continue {nameUsed}?");
+                
+                Continue = Console.ReadLine();
+                Continue = Continue.ToLower();
+
+                if (Continue != "y")
+                Console.WriteLine($"Bye {nameUsed}!!");
+                Console.ReadLine();
+
+
+
+
+            }
+                    
+        }
+    }
+}
